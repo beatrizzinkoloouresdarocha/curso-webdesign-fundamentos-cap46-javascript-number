4701089c90b91acdb70f4557ef67cd925f1a2698
Beatriz Zinko <zinkobeatriz@gmail.com>
2024-04-15 08:18:46 -0300

    concluido mais uma etapa com todas as etapas  coloquei pra rodar apareceu os dados que eu precisava .

--- introducao.js
+++ introducao.js
@@ -3,5 +3,7 @@ let numero =1234567.890
 console.log(numero.toFixed(5))
 console.log(typeof numero.toFixed(5))
 
-console.log(numero.toPrecision(3))
-console.log(typeof numero.toPrecision(3))
+console.log(numero.toPrecision(15))
+console.log(typeof numero.toPrecision(15))
+
+console.log((123).toPrecision(6))
